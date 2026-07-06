8f449da20b9be0aefba95b7ca5b0dccba48b13e4
Kirill Kolesnikov <kolesnikov-kiril@mail.ru>
2026-07-06 22:52:57 +0700

    Expand PHPStan coverage across packages

--- tests/fixtures/global/emails/welcome.php
+++ tests/fixtures/global/emails/welcome.php
@@ -1,1 +1,2 @@
+<?php /** @var string|null $user */ ?>
 <p>Welcome, <?= htmlspecialchars($user ?? 'User', ENT_QUOTES, 'UTF-8') ?></p>
--- tests/fixtures/global/home.php
+++ tests/fixtures/global/home.php
@@ -1,1 +1,2 @@
+<?php /** @var string|null $name */ ?>
 <h1>Hello, <?= htmlspecialchars($name ?? 'World', ENT_QUOTES, 'UTF-8') ?>!</h1>
